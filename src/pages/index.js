ba497fb74bc40e752f8720995a717f8242b0dabc
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-05-10 19:40:52 +0800

    Preliminary changes to test the software

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Get started under 5min ⏱️
           </Link>
         </div>
       </div>
@@ -29,7 +29,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`The FinOps Platform`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
